737fbeac7b00e007e027b7cb3e8b3cc5c81ed228
John Gosset <john@gossetx.com>
2018-09-28 09:41:54 -0400

    Use mandown to generate dude man page

--- packages.go
+++ packages.go
@@ -2,6 +2,7 @@
 
 //go:generate go build
 //go:generate upx dude
+//go:generate mandown -install . dude.1.md
 //go:generate holo-build --force --format=rpm holo.toml
 //go:generate holo-build --force --format=pacman holo.toml
 //go:generate rm dude
